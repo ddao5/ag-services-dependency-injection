6e7661c2bc61cb33a72f869db72272744dfc9f12
Duc Dao <43461796+ddao5@users.noreply.github.com>
2021-06-14 13:47:34 -0400

    'Finished'

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {LoggingService} from '../shared/logging.service';
+import {AccountsService} from '../shared/accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
+})
+export class NewAccountComponent {
+
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe((status: string) => {
+      alert('New Status: ' + status);
+    });
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+    // console.log('A server status changed, new status: ' + accountStatus);
+  }
+}
